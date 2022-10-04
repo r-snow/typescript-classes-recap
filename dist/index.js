7f80c56ad89fcd94137c2f3a8638050bdf8990e6
Ryan Snow <74696050+r-snow@users.noreply.github.com>
2022-10-04 13:31:08 -0700

    review JS static fields/methods

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,15 @@
 class Player {
+    static description = "Player description";
     #score = 0;
     #numLives = 10;
     constructor(name) {
         this.name = name;
     }
+
+    static randomPlayer() {
+        return new Player("Andy");
+    }
+
     get nickname() {
         return `${this.name}anator`;
     }
@@ -43,7 +49,7 @@ console.log(player1.score);
 player1.score = 400;
 console.log(player1.score);
 
-// console.log(player1);
+console.log(player1);
 // console.log(player1.nickname);
 // const player2 = new Player("Mike");
 // player2.taunt();
