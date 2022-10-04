ff5871f7feb02ca701eaaff48bf57dc61fae5226
Ryan Snow <74696050+r-snow@users.noreply.github.com>
2022-10-04 13:01:49 -0700

    JS class review

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,49 @@
-"use strict";
-console.log("It's working!");
+class Player {
+    #score = 0;
+    #numLives = 10;
+    constructor(name) {
+        this.name = name;
+    }
+    get nickname() {
+        return `${this.name}anator`;
+    }
+    get score() {
+        return this.#score;
+    }
+
+    set score(newScore) {
+        if (newScore < 0) {
+            throw new Error("Score must be greater than 0!");
+        }
+        this.#score = newScore;
+    }
+
+    taunt() {
+        console.log("booyah!");
+    }
+    loseLife() {
+        this.#numLives -= 1;
+    }
+    getScore() {
+        return this.#score;
+    }
+    updateScore(newScore) {
+        this.#score = newScore;
+    }
+}
+
+const player1 = new Player("Ryan");
+// console.log(player1);
+// console.log(player1.numLives);
+// player1.loseLife();
+// console.log(player1.numLives);
+// console.log(player1.score);
+// player1.updateScore(28);
+console.log(player1.score);
+player1.score = 400;
+console.log(player1.score);
+
+// console.log(player1);
+// console.log(player1.nickname);
+// const player2 = new Player("Mike");
+// player2.taunt();
