a319de115b7fd0e9a6fd0b409c4d60d1ac8a9d50
Ryan Snow <74696050+r-snow@users.noreply.github.com>
2022-10-04 14:49:41 -0700

    learning private and public

--- dist/index.js
+++ dist/index.js
@@ -1,60 +1,13 @@
+"use strict";
 class Player {
-    static description = "Player description";
-    #score = 0;
-    #numLives = 10;
-    constructor(name) {
-        this.name = name;
+    constructor(first, last) {
+        this.score = 0;
+        this.first = first;
+        this.last = last;
     }
-
-    static randomPlayer() {
-        return new Player("Andy");
-    }
-
-    get nickname() {
-        return `${this.name}anator`;
-    }
-    get score() {
-        return this.#score;
-    }
-
-    set score(newScore) {
-        if (newScore < 0) {
-            throw new Error("Score must be greater than 0!");
-        }
-        this.#score = newScore;
-    }
-
-    taunt() {
-        console.log("booyah!");
-    }
-    loseLife() {
-        this.#numLives -= 1;
-    }
-    getScore() {
-        return this.#score;
-    }
-    updateScore(newScore) {
-        this.#score = newScore;
+    secretMethod() {
+        console.log("Secret Method!");
     }
 }
-
-class AdminPlayer extends Player {
-    isAdmin = true;
-}
-
-const admin = new AdminPlayer();
-// const player1 = new Player("Ryan");
-// console.log(player1);
-// console.log(player1.numLives);
-// player1.loseLife();
-// console.log(player1.numLives);
-// console.log(player1.score);
-// player1.updateScore(28);
-// console.log(player1.score);
-// player1.score = 400;
-// console.log(player1.score);
-
-// console.log(player1);
-// console.log(player1.nickname);
-// const player2 = new Player("Mike");
-// player2.taunt();
+const sammy = new Player("Sammy", "Clark");
+sammy.secretMethod();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
+class Player {
+    readonly first: string;
+    readonly last: string;
+    private score = 0;
 
+    constructor(first: string, last: string) {
+        this.first = first;
+        this.last = last;
+    }
+
+    private secretMethod(): void {
+        console.log("Secret Method!");
+    }
+}
+
+const sammy = new Player("Sammy", "Clark");
+sammy.secretMethod();
